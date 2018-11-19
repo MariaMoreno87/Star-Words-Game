05e6fad65273bc3eccd3ec731c8c07acc49c6e34
Maria Moreno <maria.ibarra87@gmail.com>
2018-11-18 21:11:47 -0800

    all done with sound and img

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,14 +8,16 @@ var rounds = 0;
 var playerName = "";
 var totalCorrectWordsGuessed;
 var characterAudio;
-var charactersArray = [{ "name": "Chewbacca", "image": "", "sound": "./assets/sound/Chewbacca.mp3" },
-{ "name": "Jedi", "image": "", "sound": "./assets/sound/Jedi.mp3" },
-{ "name": "Sith", "image": "", "sound": "./assets/sound/Sith.mp3" },
-{ "name": "Vader", "image": "", "sound": "./assets/sound/VaderBreath.mp3" },
-{ "name": "BB8", "image": "", "sound": "./assets/sound/BB8.mp3" },
-{ "name": "C3PO", "image": "", "sound": "./assets/sound/C3PO.mp3" },
-{ "name": "R2D2", "image": "", "sound": "./assets/sound/R2D2.mp3" },
-{ "name": "Droid", "image": "", "sound": "./assets/sound/Droid.mp3" }]
+var characterImg;
+var audioCtx;
+var charactersArray = [{ "name": "Chewbacca", "image": "./assets/images/Chewbacca.jpg", "sound": "chewbaccaaudio" },
+{ "name": "Jedi", "image": "./assets/images/Jedi.jpg", "sound": "jediaudio" },
+{ "name": "Sith", "image": "./assets/images/Sith.jpg", "sound": "sithaudio" },
+{ "name": "Vader", "image": "./assets/images/Vader.jpg", "sound": "vaderaudio" },
+{ "name": "BB8", "image": "./assets/images/BB8.jpg", "sound": "bbb8audio" },
+{ "name": "C3PO", "image": "./assets/images/C3PO.jpg", "sound": "c3poaudio" },
+{ "name": "R2D2", "image": "./assets/images/R2D2.jpeg", "sound": "r2d2audio" },
+{ "name": "Droid", "image": "./assets/images/Droid.jpg", "sound": "droidaudio" }]
 
 //Get DOM elements 
 var wordElement = document.getElementById('word');
@@ -25,27 +27,27 @@ var winsCountElement = document.getElementById('winsCount');
 var losesCountElement = document.getElementById('losesCount');
 var msgBoardElement = document.getElementById('msgBoard');
 var msgElement = document.getElementById('msg');
-var myAudioElement = document.getElementById('myaudio');
-var audioSourceElement = document.getElementById('audioSource');
-var AudioContext = window.AudioContext || window.webkitAudioContext;
+var overlay = document.getElementById("overlay");
+var characterImgelement = document.getElementById('characterimg');
 
 function initializeGame() {
     if (rounds == 0) {
-        //Reset and prompt for new player
+        //Reset and prompt for new player        
         var initialRound = "0";
         winsCountElement.innerHTML = initialRound;
         losesCountElement.innerHTML = initialRound;
+        overlay.style.display = "block"//Turns on the overlay curtain
         totalCorrectWordsGuessed = [];
         playerName = prompt("Please enter your name", "Anonymous");
         msgBoardElement.innerHTML = "May the force be with you, " + playerName + "!";
     }
     lettersGuessedElement.innerHTML = "";
     word = chooseRandomCharacter(); //Get a new character that has not already been guessed correctly.
-    characterAudio = getAudioSource(word);
+    characterAudio = getAudioSource(word);//Get the audio to play for current character
+    characterImg = getCharacterImage(word);//Get the image to show for current character
     allowedGuesses = word.length + 3;
     wrongGuesses = [];
     correctGuesses = [];
-    console.log(word);
 
     // initialize correctGuesses array with underscores
     for (var i = 0; i < word.length; i++) {
@@ -57,7 +59,6 @@ function initializeGame() {
 }
 
 function updateGuesses(letter) {
-    var audioCtx = new AudioContext();
     if (!alreadyGuessed(letter)) {//Does nothing if player has already used that letter
         allowedGuesses--; // decrement guesses left
         letterCountElement.innerHTML = allowedGuesses;
@@ -89,25 +90,32 @@ function didIWin() {
         wins++;
         rounds++;
         winsCountElement.innerHTML = wins;
-        addAudioSource(characterAudio);
-        myAudioElement.play();
+        showCharacterImg(characterImg);
+        playAudio(characterAudio);
         addToMsg("Strong the force is within you, " + playerName + ". Guessed correctly, you did: " + word + "!");
         if (!isJediMaster()) {
             initializeGame();
+        } else {//player has won the whole game. Roll the credits!!!
+            showCharacterImg("./assets/images/win.gif");
+            playAudio("winaudio");
         }
-    } else if (allowedGuesses === 0) {
+
+    } else if (allowedGuesses === 0) {//player has lossed the round. select next character
         loses++;
         rounds++;
+        showCharacterImg("./assets/images/fail.gif");
         losesCountElement.innerHTML = loses;
         addToMsg("With you, the force was not, " + playerName + ". The correct word was: " + word + "!");
         initializeGame();
     }
 }
 
+//adds a message to the msg div
 function addToMsg(msg) {
     msgElement.innerHTML = msg;
 }
 
+//checks if the letter guessed has been used already
 function alreadyGuessed(guessedLetter) {
     if (wrongGuesses.indexOf(guessedLetter.toLowerCase()) > -1 || correctGuesses.indexOf(guessedLetter.toLowerCase()) > -1) {
         return true;
@@ -115,6 +123,7 @@ function alreadyGuessed(guessedLetter) {
     return false;
 }
 
+//checks if player has already guessed all the characters correctly
 function isJediMaster() {
     totalCorrectWordsGuessed.push(word);
     if (totalCorrectWordsGuessed.length === charactersArray.length) {
@@ -123,6 +132,7 @@ function isJediMaster() {
     return false;
 }
 
+//checks if the character has already been guessed correctly
 function characterGuessedAlready(potentialCharacter) {
     if (totalCorrectWordsGuessed.indexOf(potentialCharacter) > -1) {
         return true;
@@ -130,6 +140,7 @@ function characterGuessedAlready(potentialCharacter) {
     return false;
 }
 
+//selects a random character who has not already been guessed correctly
 function chooseRandomCharacter() {
     var character = randomCharacter();
     while (characterGuessedAlready(character)) {
@@ -138,27 +149,63 @@ function chooseRandomCharacter() {
     return character;
 }
 
+//selects a random character from the charactersArray
 function randomCharacter() {
     return charactersArray[Math.floor(Math.random() * charactersArray.length)].name;
 }
 
-function getAudioSource(currentCharacter) {
-    var character = charactersArray.find(character => character.name === currentCharacter);
+//get the current character sound to play
+function getAudioSource(currentCharacterSound) {
+    var character = getCurrentCharacter(currentCharacterSound);
     return character.sound;
 }
 
-function addAudioSource(currentAudioSource) {
-    audioSourceElement.src = currentAudioSource;
+//get the currenct character image to display
+function getCharacterImage(currentCharacterImg) {
+    var character = getCurrentCharacter(currentCharacterImg);
+    return character.image;
 }
 
-function playAudio(){    
-    myAudioElement.play();
+//get the selected character objec from the charactersArray
+function getCurrentCharacter(currentCharacter) {
+    // return charactersArray.find(function(character) {
+    //     character.name === currentCharacter;
+    // })
+    return charactersArray.find(character => character.name === currentCharacter);//used arrow function to cleanup code
+}
+
+function playAudio(audioId) {
+    var audioElm = getAudioElement(audioId);
+    audioElm.play();//play the HTML5 audio
+}
+
+function getAudioElement(audioElmId) {
+    return document.getElementById(audioElmId);//get the audio needed to play
+}
+
+function showCharacterImg(characterImgSrc) {
+    characterImgelement.src = characterImgSrc;//set the html image src to my character image
+}
+
+function off() {
+    overlay.style.display = "none";//turn off the overlay curtain
+    if (rounds == 0) playAudio("introaudio");//plays intro song
+}
+
+//Checks if character passed in is either a number or letter only
+function charValidation(charToValidate) {
+    if (charToValidate >= 48 && charToValidate <= 90) {
+        return true;//character IS a number or letter
+    }
+    return false;//character is NOT a number or letter
 }
 
 document.onkeyup = function (event) {
     var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
-    updateGuesses(letterGuessed);
-    didIWin();
+    if (charValidation(event.keyCode)) {//Check if key is a number or letter
+        updateGuesses(letterGuessed);
+        didIWin();
+    }
 };
 
 initializeGame();
